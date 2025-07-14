d098e5716f84f5f50de6f48dd65e79b4e5eb7682
dazai <j.szymanski0100@gmail.com>
2025-07-14 13:16:45 +0200

    v0.1.0

--- src/main.py
+++ src/main.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     print(f"\n[+] Your public IP address: {ntn.get_public_ip()}")
     print(f"[+] Your local IP address: {ntn.get_local_ip()}")
     ntn.show_connections()
+    input()
